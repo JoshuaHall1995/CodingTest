9381693e7947ff7f4da55fb9d572578591d24e35
Joshua Hall <joshua.hall@navico.com>
2020-12-09 12:17:15 +0000

    Initial test cases added

--- codingchallenge-master/ConstructionLine.CodingChallenge/SearchEngine.cs
+++ codingchallenge-master/ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace ConstructionLine.CodingChallenge
 {
@@ -11,16 +12,57 @@ namespace ConstructionLine.CodingChallenge
             _shirts = shirts;
 
             // TODO: data preparation and initialisation of additional data structures to improve performance goes here.
+            // Create pre empty ColorCount etc if needed
 
         }
 
 
         public SearchResults Search(SearchOptions options)
         {
-            // TODO: search logic goes here.
+            // Match Shirts
+            var filteredSizeOrders = _shirts.Where(o => options.Sizes.Contains(o.Size));
+            var filteredColourOrders = _shirts.Where(o => options.Colors.Contains(o.Color));
+            var matchingShirts = filteredSizeOrders.Union(filteredColourOrders).ToList();
+            
+            var colorList = new List<ColorCount>();
+            var sizeList = new List<SizeCount>();
 
+
+            var returnedSizes = matchingShirts.Select(x => x.Size)
+                .GroupBy(x => x.Name).Select(group => new { 
+                    Size = group.Key, 
+                    Count = group.Count() 
+                }).ToList();
+            
+            var returnedColours = matchingShirts.Select(x => x.Color)
+                .GroupBy(x => x.Name).Select(group => new { 
+                    Color = group.Key, 
+                    Count = group.Count() 
+                }).ToList();
+            
+            foreach (var color in Color.All)
+            {
+                colorList.Add(new ColorCount
+                {
+                    Color = color,
+                    Count = returnedColours.Count(x => x.Color == color.Name)
+                });
+            }
+            
+            foreach (var size in Size.All)
+            {
+                sizeList.Add(new SizeCount
+                {
+                    Size = size,
+                    Count = returnedSizes.Count(x => x.Size == size.Name)
+                });
+            }
+            
             return new SearchResults
             {
+                Shirts = matchingShirts,
+                SizeCounts = sizeList,
+                ColorCounts = colorList
             };
         }
     }
