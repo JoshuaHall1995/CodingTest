bf8680cbd0d4294d8c4f1416e9c558882b11a3da
Joshua Hall <joshua.hall@navico.com>
2020-12-09 13:27:33 +0000

    Tidy up

--- codingchallenge-master/ConstructionLine.CodingChallenge/SearchEngine.cs
+++ codingchallenge-master/ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -12,43 +12,34 @@ namespace ConstructionLine.CodingChallenge
             _shirts = shirts;
 
             // TODO: data preparation and initialisation of additional data structures to improve performance goes here.
-            // Create pre empty ColorCount etc if needed
-
         }
 
 
         public SearchResults Search(SearchOptions options)
         {
-            // Match Shirts
-            var filteredSizeOrders = _shirts.Where(o => options.Sizes.Contains(o.Size));
-            var filteredColourOrders = _shirts.Where(o => options.Colors.Contains(o.Color));
-            var matchingShirts = filteredSizeOrders.Union(filteredColourOrders).ToList();
-            
-            var colorList = new List<ColorCount>();
+            var matchingShirts = FindMatchingShirts(options);
+            var sizeList = MapSizes(matchingShirts);
+            var colorList = MapColours(matchingShirts);
+
+            return new SearchResults
+            {
+                Shirts = matchingShirts,
+                SizeCounts = sizeList,
+                ColorCounts = colorList
+            };
+        }
+
+        private static List<SizeCount> MapSizes(IEnumerable<Shirt> matchingShirts)
+        {
             var sizeList = new List<SizeCount>();
 
-
             var returnedSizes = matchingShirts.Select(x => x.Size)
-                .GroupBy(x => x.Name).Select(group => new { 
-                    Size = group.Key, 
-                    Count = group.Count() 
-                }).ToList();
-            
-            var returnedColours = matchingShirts.Select(x => x.Color)
-                .GroupBy(x => x.Name).Select(group => new { 
-                    Color = group.Key, 
-                    Count = group.Count() 
-                }).ToList();
-            
-            foreach (var color in Color.All)
-            {
-                colorList.Add(new ColorCount
+                .GroupBy(x => x.Name).Select(group => new
                 {
-                    Color = color,
-                    Count = returnedColours.Where(x => x.Color == color.Name).Select(x => x.Count).SingleOrDefault()
-                });
-            }
-            
+                    Size = @group.Key,
+                    Count = @group.Count()
+                }).ToList();
+
             foreach (var size in Size.All)
             {
                 sizeList.Add(new SizeCount
@@ -57,13 +48,39 @@ namespace ConstructionLine.CodingChallenge
                     Count = returnedSizes.Where(x => x.Size == size.Name).Select(x => x.Count).SingleOrDefault()
                 });
             }
-            
-            return new SearchResults
+
+            return sizeList;
+        }
+
+        private static List<ColorCount> MapColours(IEnumerable<Shirt> matchingShirts)
+        {
+            var colorList = new List<ColorCount>();
+
+            var returnedColours = matchingShirts.Select(x => x.Color)
+                .GroupBy(x => x.Name).Select(group => new
+                {
+                    Color = @group.Key,
+                    Count = @group.Count()
+                }).ToList();
+
+            foreach (var color in Color.All)
             {
-                Shirts = matchingShirts,
-                SizeCounts = sizeList,
-                ColorCounts = colorList
-            };
+                colorList.Add(new ColorCount
+                {
+                    Color = color,
+                    Count = returnedColours.Where(x => x.Color == color.Name).Select(x => x.Count).SingleOrDefault()
+                });
+            }
+
+            return colorList;
+        }
+
+        private List<Shirt> FindMatchingShirts(SearchOptions options)
+        {
+            var filteredSizeOrders = _shirts.Where(o => options.Sizes.Contains(o.Size));
+            var filteredColourOrders = _shirts.Where(o => options.Colors.Contains(o.Color));
+            var matchingShirts = filteredSizeOrders.Union(filteredColourOrders).ToList();
+            return matchingShirts;
         }
     }
 }
